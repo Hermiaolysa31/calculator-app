f491a4d412fba7b0e13e90bdeecf323156f82bdb
Hermiaolysa31 <hermiaolysa@students.amikom.ac.id>
2023-03-15 11:47:52 +0700

    diisi dengan keterangan commit

---
+++ Calculator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello wolrd!");
+            MethodHelloWorld();
+        }
+
+        static void MethodHelloWorld()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
